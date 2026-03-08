a1dbca2510b14a387b3cb463f4df2f5e53cc133b
kehan-nn <keihanmagic@gmail.com>
2026-03-09 00:55:00 +0700

    Menambahkan tampiln GUI kalkulator

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,20 @@ namespace TP_MODUL3_103022400012
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
